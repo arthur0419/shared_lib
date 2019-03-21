dcf902f488591a970569a9febd91353b4b0de83f
linl@corp.jadenine.com <linl@corp.jadenine.com>
2019-03-21 16:04:28 +0800

    test

--- vars/cambricon.groovy
+++ vars/cambricon.groovy
@@ -1,21 +1,7 @@
 def call(Map config) {
-        script {
-            def debug = true
-            echo 'Cambricon Config:'
-            config.each{
-                println(it.key + " : " + it.value)
-                // echo it.key + " : " + it.value
-            }
-            if (config.debug) {
-                debug = config.debug
-            }
-            echo "${debug}"
-            echo "${config.debug}"
-            echo 'do somthing in Cambricon'
-        }
-        // do something for cambricon
-        // echo 'Cambricon Config:'
         // script {
+        //     def debug = true
+        //     echo 'Cambricon Config:'
         //     config.each{
         //         println(it.key + " : " + it.value)
         //         // echo it.key + " : " + it.value
@@ -23,16 +9,15 @@ def call(Map config) {
         //     if (config.debug) {
         //         debug = config.debug
         //     }
+        //     echo "${debug}"
+        //     echo "${config.debug}"
+        //     echo 'do somthing in Cambricon'
         // }
-        // // echo "${debug}"
-        // echo "${config.debug}"
-
-        // echo 'Cambricon build config:'
-        // buildconfig.each{
-        //     println(it.key + " : " + it.value)
-        //     // echo it.key + " : " + it.value
-        // }
-        
-        // echo 'do somthing in Cambricon'
+        stage('build') {
+            steps {
+                echo 'cambricon build!'
+                helloworld 'jenkins'
+            }
+        }
     
     } 
