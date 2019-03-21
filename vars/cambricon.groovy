24d538e2f89179ab2d4f1472861cc8b5ad5c1cb2
linl@corp.jadenine.com <linl@corp.jadenine.com>
2019-03-21 17:06:12 +0800

    test

--- vars/cambricon.groovy
+++ vars/cambricon.groovy
@@ -14,21 +14,31 @@ def call(Map config) {
         //     echo 'do somthing in Cambricon'
         // }
 
-        pipeline {
-            agent any
-            stages {
-                stage('build') {
-                    steps {
-                        echo "doing cambricon build"
-                    }
-                }
-                stage('test') {
-                    when {
-                        expression {return config.test}
-                    }
-                    steps {
-                        echo "doing cambricon test"
-                    }
+        // pipeline {
+        //     agent any
+        //     stages {
+        //         stage('build') {
+        //             steps {
+        //                 echo "doing cambricon build"
+        //             }
+        //         }
+        //         stage('test') {
+        //             when {
+        //                 expression {return config.test}
+        //             }
+        //             steps {
+        //                 echo "doing cambricon test"
+        //             }
+        //         }
+        //     }
+        // }
+
+        node {
+            stage('build') {
+                if (config.debug) {
+                    echo 'doing build debug version'
+                } else {
+                    echo 'doing build release version'
                 }
             }
         }
