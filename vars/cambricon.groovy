c8d32ed5538808505b21f1e241712b053b2e4dbf
linl@corp.jadenine.com <linl@corp.jadenine.com>
2019-03-28 16:59:01 +0800

    remove useless source code

--- vars/cambricon.groovy
+++ vars/cambricon.groovy
@@ -1,38 +1,4 @@
 def call(Closure callable) {
-        // script {
-        //     def debug = true
-        //     echo 'Cambricon Config:'
-        //     config.each{
-        //         println(it.key + " : " + it.value)
-        //         // echo it.key + " : " + it.value
-        //     }
-        //     if (config.debug) {
-        //         debug = config.debug
-        //     }
-        //     echo "${debug}"
-        //     echo "${config.debug}"
-        //     echo 'do somthing in Cambricon'
-        // }
-
-        // pipeline {
-        //     agent any
-        //     stages {
-        //         stage('build') {
-        //             steps {
-        //                 echo "doing cambricon build"
-        //             }
-        //         }
-        //         stage('test') {
-        //             when {
-        //                 expression {return config.test}
-        //             }
-        //             steps {
-        //                 echo "doing cambricon test"
-        //             }
-        //         }
-        //     }
-        // }
-
         def config = [:]
         callable.resolveStrategy = Closure.DELEGATE_FIRST;
         callable.delegate = config;
