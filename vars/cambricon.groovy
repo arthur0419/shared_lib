ea9a0f4bacb1d7e5c8ea2d9850c7416ef184c88f
linl@corp.jadenine.com <linl@corp.jadenine.com>
2019-03-28 17:06:11 +0800

    fix tab

--- vars/cambricon.groovy
+++ vars/cambricon.groovy
@@ -1,114 +1,113 @@
 def call(Closure callable) {
-        def config = [:]
-        callable.resolveStrategy = Closure.DELEGATE_FIRST;
-        callable.delegate = config;
-        callable.call()
+    def config = [:]
+    callable.resolveStrategy = Closure.DELEGATE_FIRST;
+    callable.delegate = config;
+    callable.call()
 
-        def build = null
+    def build = null
 
-        if (config.build) {
-            build = [:]
-            config.build.resolveStrategy = Closure.DELEGATE_FIRST
-            config.build.delegate = build
-            config.build()
+    if (config.build) {
+        build = [:]
+        config.build.resolveStrategy = Closure.DELEGATE_FIRST
+        config.build.delegate = build
+        config.build()
+    }
+    def test = null
+    if (config.test) {
+        test = [:]
+        config.test.resolveStrategy = Closure.DELEGATE_FIRST
+        config.test.delegate = test
+        config.test()
+    }
+
+    node {
+        stage('prepare') {
+            echo 'doing prepare'
+            println("Cambricon Config:");
+            config.each {
+                println(it.key + " : " + it.value)
+            }
         }
-        def test = null
-        if (config.test) {
-            test = [:]
-            config.test.resolveStrategy = Closure.DELEGATE_FIRST
-            config.test.delegate = test
-            config.test()
-        }
-
-        node {
-            stage('prepare') {
-                echo 'doing prepare'
-                println("Cambricon Config:");
-                config.each {
+            
+        if (build) {
+            stage('build') {
+                if (build.debug) {
+                    echo 'doing build debug version'
+                } else {
+                    echo 'doing build release version'
+                }
+                println("Build Config:");
+                build.each {
                     println(it.key + " : " + it.value)
                 }
             }
+        }
             
-            if (build) {
-                stage('build') {
-                    if (build.debug) {
-                        echo 'doing build debug version'
-                    } else {
-                        echo 'doing build release version'
-                    }
+        if (test) {
+            stage('test') {
+                // echo 'doing test'
+                // println("Test Config:");
+                // test.each {
+                //     println(it.key + " : " + it.value)
+                // }
 
-                    println("Build Config:");
-                    build.each {
-                        println(it.key + " : " + it.value)
-                    }
+                def count = 0
+                if (test.caffe) {
+                    count++
                 }
-            }
-            
-            if (test) {
-                stage('test') {
-                    // echo 'doing test'
-                    // println("Test Config:");
-                    // test.each {
-                    //     println(it.key + " : " + it.value)
-                    // }
-
-                    def count = 0
+                if (test.tensorflow) {
+                    count++
+                }
+                if (count == 1) {
                     if (test.caffe) {
-                        count++
+                        stage('caffe') {
+                            echo 'doing caffe test'
+                        }
                     }
                     if (test.tensorflow) {
-                        count++
+                        stage('tensorflow') {
+                            echo 'doing tensorflow test'
+                        }
                     }
-                    if (count == 1) {
-                        if (test.caffe) {
-                            stage('caffe') {
+                } 
+
+                if (count == 2) {
+                    def buildStages = [:]
+                    buildStages.put('caffe', {
+                        stage('caffe') {
+                            stage('caffe_build') {
+                                echo 'doing caffe build'
+                            }
+                            stage('caffe_test') {
                                 echo 'doing caffe test'
                             }
                         }
-                        if (test.tensorflow) {
-                            stage('tensorflow') {
+                    })
+                    buildStages.put('tensorflow', {
+                        stage('tensorflow') {
+                            stage('tensorflow_build') {
+                                echo 'doing tensorflow build'
+                            }
+                            stage('tensorflow_test') {
                                 echo 'doing tensorflow test'
                             }
                         }
-                    } 
+                    })
 
-                    if (count == 2) {
-                        def buildStages = [:]
-                        buildStages.put('caffe', {
-                            stage('caffe') {
-                                stage('caffe_build') {
-                                    echo 'doing caffe build'
-                                }
-                                stage('caffe_test') {
-                                    echo 'doing caffe test'
-                                }
-                            }
-                        })
-                        buildStages.put('tensorflow', {
-                            stage('tensorflow') {
-                                stage('tensorflow_build') {
-                                    echo 'doing tensorflow build'
-                                }
-                                stage('tensorflow_test') {
-                                    echo 'doing tensorflow test'
-                                }
-                            }
-                        })
-
-                        parallel(buildStages)
-                    }
-
-                    // parallel {
-                    //     stage('caffe') {
-                    //         echo 'doing caffe test'
-                    //     }
-                    //     stage('tensorflow') {
-                    //         echo 'doing tensorflow test'
-                    //     }
-                    // }
-
+                    parallel(buildStages)
                 }
+
+                // parallel {
+                //     stage('caffe') {
+                //         echo 'doing caffe test'
+                //     }
+                //     stage('tensorflow') {
+                //         echo 'doing tensorflow test'
+                //     }
+                // }
+
             }
         }
-    
-    } 
+    }
+
+} 
